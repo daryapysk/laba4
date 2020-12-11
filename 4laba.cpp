94cdb517323e52780c03cfe96b0aa48177a205cd
OlyaOstrovskaya <56983605+OlyaOstrovskaya@users.noreply.github.com>
2020-12-12 01:31:13 +0300

    Update 4laba.cpp

--- 4laba.cpp
+++ 4laba.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <iterator>
@@ -7,7 +7,24 @@
 using std::vector;
 using namespace std;
 
+template <typename T>
+void gnomeSort(T& arr, int n)  
+{
+	int index = 0;
+	while (index < n) {
+		if (index == 0) index++;
+		else if (arr[index] >= arr[index - 1]) index++;
+		else {
+			swap(arr[index], arr[index - 1]);
+			index--;
+		}
+	}
+	return;
+}
+
+
 int main() 
 {
+  
 }
 
