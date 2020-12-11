62e3586d459f381298744fde295a196489d5114e
OlyaOstrovskaya <56983605+OlyaOstrovskaya@users.noreply.github.com>
2020-12-12 01:54:23 +0300

    Update 4laba.cpp

--- 4laba.cpp
+++ 4laba.cpp
@@ -22,6 +22,44 @@ void gnomeSort(T& arr, int n)
 	return;
 }
 
+template <typename T1, typename T2>
+void pigeonholeSort(T1& arr, T2 n)   
+{
+	
+	T2 min = arr[0], max = arr[0];
+	for (int i = 1; i < int(n); i++) {
+		if (min > arr[i])
+			min = arr[i];
+		if (max < arr[i])
+			max = arr[i];
+	}
+	T2 range = max - min;  
+	range++;
+
+	
+	vector <vector<T2>> holes;
+	vector <T2> temp;
+	temp.clear();
+
+	for (T2 i = 0; i < range; i++) {
+		holes.push_back(temp);
+	}
+
+	for (int i = 0; i < n; i++) {
+		holes[arr[i] - min].push_back(arr[i]);
+	}
+
+	int index = 0;
+	for (T2 i = 0; i < range; i++) {
+		for (int j = 0; j < holes[i].size(); j++) {
+			arr[index] = holes[i][j];
+			index++;
+		}
+	}
+
+	return;
+}
+
 
 int main() 
 {
